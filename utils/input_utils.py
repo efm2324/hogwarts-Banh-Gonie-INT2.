442302cd766bd72672c294d193d955091d5a1833
efm2324 <gonie.louis@gmail.com>
2025-11-28 23:14:01 +0100

    .

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -7,18 +7,23 @@ def ask_text(message):
             print("Input cannot be empty. Please try again.")
 
 def ask_number(message, min_value=None, max_value=None):
-    courage_lv = int(input(message))
-    while courage_lv < min_value or courage_lv > max_value:
-        print(f"Please enter a number between {min_value} and {max_value}.")
-        courage_lv = int(input(message))
-    return courage_lv
+    while True:
+        try:    
+            courage_lv = int(input(message))
+        except ValueError:
+            print("enter a valid number.")
+            continue 
+        while courage_lv < min_value or courage_lv > max_value:
+            print(f"Please enter a number between {min_value} and {max_value}.")
+            courage_lv = int(input(message))
+        return courage_lv
 
 def ask_choice(message, option):
     print(message)
-    for i, option in enumerate(option, start=1):
+    for i, option in enumerate(option, 1):
         print(f"{i}. {option}")
 
     choice = ask_number("Your choice: ", 1, len(option))
     return option[choice - 1]
 
-ask_choice("dotou want to continue?", ["yes", "no"])
+def load_file(file_path):
